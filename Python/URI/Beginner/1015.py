1c0b7dd417b84aa411ad2ec99fd066564f01b77f
mangueBit <alm3@cin.ufpe.br>
2017-11-04 11:12:53 -0300

    new prob solved

--- Python/URI/Beginner/1015.py
+++ Python/URI/Beginner/1015.py
@@ -4,10 +4,10 @@ from math import *
 x1, y1 = input().split()
 x2, y2 = input().split()
 
-float(x1)
-float(y1)
-float(x2)
-float(y2)
+x1 = float(x1)
+y1 = float(y1)
+x2 = float(x2)
+y2 = float(y2)
 
 d = sqrt(pow((x2-x1),2) + pow((y2-y1),2))
 
