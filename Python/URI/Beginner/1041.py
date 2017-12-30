64f7fb67a7a3b6c705ef12cfc0ae6518967090fb
mangueBit <alm3@cin.ufpe.br>
2017-12-29 22:04:55 -0300

    coordinates solved

---
+++ Python/URI/Beginner/1041.py
@@ -0,0 +1,21 @@
+import sys
+
+x, y = input().split()
+
+x = float(x)
+y = float(y)
+
+if (x > 0 and y > 0):
+	print ("Q1")
+elif (x > 0 and y < 0):
+	print ("Q4")
+elif (x < 0 and y > 0):
+	print ("Q2")
+elif (x < 0 and y < 0):
+	print ("Q3")
+elif (x == 0 and y != 0):
+	print ("Eixo Y")
+elif (x != 0 and y == 0):
+	print ("Eixo X")
+else:
+	print ("Origem")
