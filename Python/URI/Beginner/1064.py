ec4d0b2172fd34852c95b01d6ccfc032498b1882
mangueBit <alm3@cin.ufpe.br>
2018-01-04 13:21:14 -0300

    positives and average

---
+++ Python/URI/Beginner/1064.py
@@ -0,0 +1,15 @@
+#positives and average
+
+ct = 0
+total = 0
+
+for i in range(1,7):
+	x = input()
+	x = float(x)
+
+	if (x > 0):
+		ct+=1
+		total+=x
+
+print("%d valores positivos" % ct)
+print("%.1f" % (total/ct))
