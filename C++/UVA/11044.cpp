e60f4ac4be2378479c62754abeb9913b2032c122
André Luna <alm3@cin.ufpe.br>
2018-03-29 15:32:27 -0300

    searching for nessy

---
+++ C++/UVA/11044.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+	int tc, n, m;
+	scanf("%d",&tc);
+
+	while (tc--){
+		scanf("%d%d",&n,&m);
+		printf("%d\n",(n/3)*(m/3));
+	}
+
+	return 0;
+}
