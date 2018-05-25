d64e65b9b387349e07aec55c75fa6571c7547710
André Luna <alm3@cin.ufpe.br>
2018-05-25 11:09:28 -0300

    wrong subtraction

---
+++ OBI-training/list 1/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+	int n, k;
+
+	scanf("%d%d",&n,&k);
+
+	while (k--){
+		if ((n%10) != 0) n--;
+		else n /= 10;
+	}
+
+	printf("%d\n",n );
+
+	return 0;
+}
