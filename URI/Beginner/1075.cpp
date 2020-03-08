358e7b1172a9d93627dfeb5d37d2ddbb3590d66b
mangueBit <alm3@cin.ufpe.br>
2020-03-08 16:46:01 -0300

    remaining 2

---
+++ URI/Beginner/1075.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    scanf("%d",&n);
+
+    for (int i=1;i<=10000;i++){
+        if (i%n==2){
+            printf("%d\n",i);
+        }
+    }
+    
+    return 0;
+}
