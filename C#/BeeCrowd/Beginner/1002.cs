d7b65cb4383a523e8f6777a02384fbd5d511ac38
Andre Luna <alm3@cin.ufpe.br>
2023-03-07 02:49:02 -0300

    Area of a Circle

---
+++ C#/BeeCrowd/Beginner/1002.cs
@@ -0,0 +1,33 @@
+﻿using System; 
+using System.Globalization;
+
+class URI {
+    public static string AddsLastZeroToMissingResults(string areaOfCircle) {
+       
+        var numberOfDigitsAfterDecimalPoint = 
+            (areaOfCircle.Length) -
+            (areaOfCircle.Substring(0, areaOfCircle.LastIndexOf('.') + 1).Length);
+
+        if (numberOfDigitsAfterDecimalPoint == 3) {
+            areaOfCircle += '0';
+        }
+
+        return areaOfCircle;        
+    }
+
+    // trick problem because you have to deal with different float precision separators (',', '.')
+    static void Main(string[] args) {  
+
+        const double pi = 3.14159;
+
+        var radius = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        var areaOfCircle = Math.Round(pi * Math.Pow(radius, 2), 4);
+
+        var areaOfCircleWithDotSeparator = areaOfCircle.ToString(
+            CultureInfo.CreateSpecificCulture("en-GB"));        
+
+        Console.WriteLine($"A={AddsLastZeroToMissingResult(areaOfCircleWithDotSeparator)}");
+    }
+
+}
