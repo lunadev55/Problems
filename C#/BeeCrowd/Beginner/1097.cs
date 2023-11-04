4a62fea057cbb96c96e7174989a320f7060e2227
André Marinho <andre.marinho@equipeblue.com.br>
2023-11-04 15:32:54 -0300

    sequence ij 3

---
+++ C#/BeeCrowd/Beginner/1097.cs
@@ -0,0 +1,23 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int i = 1;
+        int j = 7;
+        int counter = 0;
+
+        while (i < 10) {
+            Console.WriteLine($"I={i} J={j - counter}");
+            counter++;
+
+            if (counter == 3) {   
+                i += 2;      
+                j += 2;
+                counter = 0;
+            }
+        }
+    }
+
+}
