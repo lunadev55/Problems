d39ed0cf389a6b8f28a0570fd8759abac01f61b5
André Marinho <andre.marinho@equipeblue.com.br>
2023-11-03 22:08:42 -0300

    score validation

---
+++ C#/BeeCrowd/Beginner/1117.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+class URI {
+    static void Main(string[] args) {
+
+        double scoreA = Convert.ToDouble(Console.ReadLine());
+        while (scoreA < 0 || scoreA > 10) {
+            Console.WriteLine("nota invalida");
+            scoreA = Convert.ToDouble(Console.ReadLine());
+        }      
+
+        double scoreB = Convert.ToDouble(Console.ReadLine());
+        while (scoreB < 0 || scoreB > 10) {
+            Console.WriteLine("nota invalida");
+            scoreB = Convert.ToDouble(Console.ReadLine());
+        }
+        
+        double average = (scoreA + scoreB)  / 2;
+        Console.WriteLine($"media = { average }");    
+        return;      
+    }
+}
