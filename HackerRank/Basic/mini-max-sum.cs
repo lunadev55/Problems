4dfe164a4950de5ecfb4901fdf566cf4d78072eb
Andre Luna <alm3@cin.ufpe.br>
2024-03-20 05:46:53 -0300

    mini max sum

---
+++ HackerRank/Basic/mini-max-sum.cs
@@ -0,0 +1,45 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        arr.Sort();
+        
+        long minSum = (long) arr[0] + arr[1] + arr[2] + arr[3];
+        long maxSum = (long) arr[1] + arr[2] + arr[3] + arr[4];
+        
+        Console.WriteLine($"{minSum} {maxSum}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
