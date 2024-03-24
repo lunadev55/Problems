b40e209103179bdf5ea6876677012eab1003858e
Andre Luna <alm3@cin.ufpe.br>
2024-03-24 10:55:14 -0300

    subarray-division-1

---
+++ HackerRank/Basic/subarray-division-1.cs
@@ -0,0 +1,72 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'birthday' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY s
+     *  2. INTEGER d
+     *  3. INTEGER m
+     */
+
+    public static int birthday(List<int> s, int d, int m)
+    {
+        int numberOfPossibleSegments = 0;
+        
+        for (int i = 0; i <= s.Count - m; i++)
+        {
+            int sumOfValues = 0;
+            
+            for (int j = i; j < i + m; j++)
+            {
+                sumOfValues += s[j];
+            }
+            
+            if (sumOfValues == d)
+                numberOfPossibleSegments++;
+        }
+        return numberOfPossibleSegments;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int d = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        int result = Result.birthday(s, d, m);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
