6dc163ba0140f4ef21c2a14712b626cf87bca3fa
Andre Luna <alm3@cin.ufpe.br>
2024-03-24 01:24:32 -0300

    single number

---
+++ LeetCode/Easy/single-number.cs
@@ -0,0 +1,13 @@
+
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        int result = 0;
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            result ^= nums[i];
+        }
+
+        return result;
+    }
+}
