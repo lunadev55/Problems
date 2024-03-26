182b3ef2fd8de171878c3f90c6fb1f164d223b5a
Andre Luna <alm3@cin.ufpe.br>
2024-03-26 18:11:09 -0300

    merge two sorted lists

---
+++ LeetCode/Easy/21.cs
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode headOfMergedList = new ListNode(0);
+        ListNode current = headOfMergedList;
+
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val < list2.val)
+            {
+                current.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 == null)
+        {
+            current.next = list2;
+        }
+        else
+        {
+            current.next = list1;
+        }
+
+        return headOfMergedList.next;
+    }
+}
