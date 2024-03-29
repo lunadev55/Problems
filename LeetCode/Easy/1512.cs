9a668c306b3046aab8566ea4d4f44a9c516ca736
Andre Luna <alm3@cin.ufpe.br>
2024-03-29 19:31:14 -0300

    number of good pairs

---
+++ LeetCode/Easy/1512.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int NumIdenticalPairs(int[] nums) {
+        int goodPairs = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j] && i < j)
+                    goodPairs++;                
+            }
+        }
+        return goodPairs;
+    }
+}
