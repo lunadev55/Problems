7af9b161a680180f86dc16bee1f16e6629a8ce3d
Andre Luna <alm3@cin.ufpe.br>
2024-10-14 13:41:34 -0300

    several scores with validation

--- C#/BeeCrowd/Beginner/1118.cs
+++ C#/BeeCrowd/Beginner/1118.cs
@@ -1,56 +1,40 @@
-using System;
-using System.Collections.Specialized;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Net.NetworkInformation;
-using System.Runtime.Serialization;
-using System.Security.Cryptography.X509Certificates;
-using System.Reflection.Metadata.Ecma335;
-
-class URI
-{
-    // returning incorrect output (i guess it might be related to float precision)
-    public static void Main(string[] args) 
-    {
-        Console.WriteLine("novo calculo (1-sim 2-nao)");
-        int option = Convert.ToInt32(Console.ReadLine());
-
-        while (option is not 2)
-        {
-            double scoreA; double scoreB;
-
-            scoreA = GetScoreAsDouble(Console.ReadLine());
-            while (!IsValidScore(scoreA))
-            {
-                Console.WriteLine("nota invalida");
-                scoreA = GetScoreAsDouble(Console.ReadLine());
-            }
-
-            scoreB = GetScoreAsDouble(Console.ReadLine());  
-            while (!IsValidScore(scoreB))
-            {
-                Console.WriteLine("nota invalida");
-                scoreB = GetScoreAsDouble(Console.ReadLine());
-            }
-
-            Console.WriteLine($"media = {Media(scoreA, scoreB):F2}");       
-        }    
-    }   
-
-    public static double Media(double scoreA, double scoreB)
-    {
-        return (scoreA + scoreB) / 2;
-    }
-
-    public static bool IsValidScore(double score)
+using System; 
+class URI {
+    static bool IsValidScore(float score)
     {
         if (score < 0 || score > 10)
             return false;
         return true;
     }
+    static void Main(string[] args) {
+        while (true)
+        {
+            float scoreA = float.Parse(Console.ReadLine());
+            while (!IsValidScore(scoreA))
+            {
+                Console.WriteLine("nota invalida");
+                scoreA = float.Parse(Console.ReadLine());
+            }
 
-    public static double GetScoreAsDouble(string score)
-    {
-        return Convert.ToDouble(score);
+            float scoreB = float.Parse(Console.ReadLine());        
+            while (!IsValidScore(scoreB))
+            {
+                Console.WriteLine("nota invalida");
+                scoreB = float.Parse(Console.ReadLine());
+            }
+
+            float average = (scoreA + scoreB) / 2;            
+            Console.WriteLine($"media = {average:F2}");
+            
+            int input = 0;
+            while (input != 1 && input != 2)
+            {
+                Console.WriteLine("novo calculo (1-sim 2-nao)");
+                input = int.Parse(Console.ReadLine());                
+            }
+
+            if (input == 2)
+                break;
+        }        
     }
 }
