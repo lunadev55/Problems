9e0f79de86252bb2a6927828067ba29daef616d1
André Luna <alm3@cin.ufpe.br>
2025-06-15 23:54:47 -0300

    hot dogs

---
+++ C#/BeeCrowd/Beginner/2234.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Diagnostics.Contracts;
+using System.Runtime.ExceptionServices;
+class URI {
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();        
+
+        decimal numberOfConsumedHotDogs = Convert.ToDecimal(input.Split(' ')[0]);
+        decimal numberOfParticipants = Convert.ToDecimal(input.Split(' ')[1]);
+
+        decimal result = (decimal)(numberOfConsumedHotDogs / numberOfParticipants);
+
+        Console.WriteLine($"{result:F2}");        
+    }          
+}
