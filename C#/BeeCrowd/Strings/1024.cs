69da8771496b07fbde9d77cfe06c3821ea45bd8e
André Luna <alm3@cin.ufpe.br>
2025-08-20 15:12:40 -0300

    encryption

---
+++ C#/BeeCrowd/Strings/1024.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+class URI
+{
+    static void Main(string[] args)
+    {        
+        int testCaseNumber = int.Parse(Console.ReadLine());
+
+        while (testCaseNumber > 0)
+        {
+            var message = Console.ReadLine();
+
+            var messageStringBuilder = new StringBuilder(message);
+
+            for (int i = 0; i < messageStringBuilder.Length; i++)
+            {
+                if (char.IsLetter(messageStringBuilder[i]))                
+                    messageStringBuilder[i] = (char)(messageStringBuilder[i] + 3);                
+            }
+                        
+            var reversedString = new StringBuilder(
+                new string(messageStringBuilder.ToString().ToCharArray().Reverse().ToArray())
+            );    
+
+            for (int i = reversedString.Length / 2; i < reversedString.Length; i++)
+            {
+                reversedString[i] = (char)(reversedString[i] - 1);
+            }      
+
+            Console.WriteLine(reversedString);
+
+            testCaseNumber--;
+        } 
+    }          
+}
